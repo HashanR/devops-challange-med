59357b5c1fceb34d999d3df93febff680b9dd94d
HashanR <rmhashan@gmail.com>
2025-02-17 22:14:44 +0000

    Add helm provider and kubectl proviers

--- providers.tf
+++ providers.tf
@@ -4,10 +4,38 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
+
   required_version = ">= 1.0.0"
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
 }
