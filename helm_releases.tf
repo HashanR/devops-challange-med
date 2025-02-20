57c375d5a9cd2991b8d437d832bbb57b1a73f092
HashanR <rmhashan@gmail.com>
2025-02-20 05:19:11 +0000

    Final working solution

--- helm_releases.tf
+++ helm_releases.tf
@@ -22,6 +22,52 @@ resource "helm_release" "nginx_ingress" {
   }
 
 }
+
+########################################################
+#                                                      #
+#                      Cert Manager                    #
+########################################################
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "v1.17.0"
+  namespace  = "cert-manager"
+  create_namespace = true
+  timeout    = 600 # 10 minutes
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+}
+########################################################
+#                                                      #
+#                   Let's Encrypt Issuer               #
+########################################################
+resource "kubectl_manifest" "letsencrypt_cluster_issuer" {
+  depends_on = [helm_release.cert_manager]
+
+  yaml_body = <<YAML
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: letsencrypt-prod
+spec:
+  acme:
+    email: rmhashan@gmail.com
+    server: https://acme-v02.api.letsencrypt.org/directory
+    privateKeySecretRef:
+      name: letsencrypt-prod
+    solvers:
+    - http01:
+        ingress:
+          class: nginx
+YAML
+}
+
+
 ########################################################
 #                                                      #
 #                    Kube Prom Stack                   #
@@ -55,6 +101,15 @@ resource "helm_release" "kube_prometheus_stack" {
     name  = "grafana.ingress.annotations.kubernetes\\.io/ingress\\.class"
     value = "nginx"
   }
-
+  
+  set {
+  name  = "grafana.ingress.tls[0].secretName"
+  value = "letsencrypt-prod"
 }
 
+set {
+  name  = "grafana.ingress.tls[0].hosts[0]"
+  value = "grafana.hashanr.cloud"
+}
+
+}
